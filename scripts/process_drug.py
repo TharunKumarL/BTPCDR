f93621f55a8e4157a1458caaeff1888809558a1d
SREEKANTHREDDY-SR <sreekanth00kumar11reddy@gmail.com>
2025-04-01 00:24:43 +0530

    commit

--- scripts/process_drug.py
+++ scripts/process_drug.py
@@ -4,8 +4,9 @@ from rdkit import Chem
 import hickle as hkl
 
 # Define paths
-drug_smiles_file = '../data/223drugs_pubchem_smiles.txt'
-save_dir = '../data/GDSC/drug_graph_feat'
+script_dir = os.path.dirname(os.path.abspath(__file__))
+drug_smiles_file = os.path.join(script_dir, '../data/223drugs_pubchem_smiles.txt')
+save_dir = os.path.join(script_dir, '../data/GDSC/drug_graph_feat')
 
 # Read SMILES data
 pubchemid2smile = {line.split('\t')[0]: line.split('\t')[1].strip() for line in open(drug_smiles_file).readlines()}
